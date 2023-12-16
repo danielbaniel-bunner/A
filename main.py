393a255d29053e430d3d19d3f59399f329f4f781
danielbaniel-bunner <dlaufer_111@hotmail.com>
2023-12-16 15:23:40 +0200

    j

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import selenium
+import pandas as pd
+
+# Replace 'your_file.xlsx' with the path to your Excel file
+df = pd.read_excel('/Users/momo/A/account.xls')
+
+# This will display the first few rows of the file
+print(df.head())
